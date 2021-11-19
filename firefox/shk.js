f08098ce1bda52b36427162afab112a0bb58e41e
JerryPopi <45138831+JerryPopi@users.noreply.github.com>
2021-11-19 12:10:28 +0200

    Gaming moment (2021 LEGIT ROBLOX VBUX HACK)

--- firefox/shk.js
+++ firefox/shk.js
@@ -16,23 +16,3 @@ for (let i = 0; i < dashboardStats.length; i++) {
 }
 document.querySelector(".highcharts-background").style.fill = "#363636";
 
-// document.addEventListener("load", () => {
-    var imgs = document.querySelectorAll(".avatar");
-    console.log(imgs.length);
-    for (var i = 0; i < imgs.length; i++) {
-        // console.log(element.src);
-        // if (element.src === "https://app.shkolo.bg/img/noavatar.png") {
-        imgs[i].src =
-            "https://dotnet.microsoft.com/static/images/illustrations/swimlane-csharp-javascript.svg";
-        // }
-    }
-// });
-
-// window.addEventListener("load", () => {
-// 	setTimeout(() => {
-// 	alert("cock")
-
-// 	// stuff that happens for each web page load goes here
-
-// }, 5000)
-// });
--- firefox/test.js
+++
@@ -1,9 +0,0 @@
-var imgs = document.getElementsByTagName("img");
-	console.log(imgs.length);
-	for (var i = 0; i < imgs.length; i++) {
-		console.log(imgs[i].src);
-		if (imgs[i].src === "https://app.shkolo.bg/img/noavatar.png") {
-			imgs[i].src =
-			"https://c.tenor.com/kHcmsxlKHEAAAAAM/rock-one-eyebrow-raised-rock-staring.gif";
-		}
-	}
